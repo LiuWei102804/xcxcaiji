c218578559e44ece16674cfe4957b306fd790a71
liuwei <wei.liu@g-town.com.cn>
2019-01-04 18:06:09 +0800

    小程序菜鸡

---
+++ src/util/info.js
@@ -0,0 +1,4 @@
+export const WINWIDTH = wx.getSystemInfoSync().windowWidth;
+export const WINHEIGHT = wx.getSystemInfoSync().windowHeight;
+
+export const RPX = wx.getSystemInfoSync().windowWidth / 750;
