5a104de7053012f6478bdfec6a1b256dad898604
刘为 <wei.liu@g-town.com.cn>
2019-03-07 18:05:57 +0800

    地址查询

--- src/mixins/httpOrigin.js
+++ src/mixins/httpOrigin.js
@@ -5,4 +5,19 @@ export default class HttpOrigin extends wepy.mixin {
     data = {
         httpOrigin : "http://localhost:8999"
     }
+    methods = {
+
+    }
+    pageTo( page ) {
+        wx.navigateTo({
+            url : page
+        });
+    }
+    setData( key , data ){
+        this[key] = data;
+        console.log( this[key])
+    }
+    getData( key ){
+        return this[key];
+    }
 }
